f0afcc337296231b54c1e1fe0fcc9f38e4ac4d52
noneks <kirill_lp@list.ru>
2013-11-06 12:20:49 +0700

    Finaly update

--- ClassBuble.rb
+++ ClassBuble.rb
@@ -4,10 +4,11 @@ class Sort
   def initialize(array=[])
     @array=array
   end
+    
   def bubleSort
-    arraySize=@array.length
-    for i in 0...arraySize do
-      for j in 0...(arraySize-i-1) do
+    array_size=@array.length
+    for i in 0...array_size do
+      for j in 0...(array_size-i-1) do
         if @array[j] > @array[j+1]
            @array[j], @array[j+1] = @array[j+1], @array[j]
          end
@@ -15,27 +16,39 @@ class Sort
     end
     @array
   end
- end
- def randomArray(array)
-   p "Enter size: "
-   array4randomArray=gets.chomp
-   p "Enter random range from:"
-   leftRange=gets.chomp
-   p "to:"
-   rightRange=gets.chomp
-   
-   array4randomArray=array4randomArray.to_i
-   leftRange=leftRange.to_i
-   rightRange=rightRange.to_i
-   
-     array4randomArray.times do
-     array.<< rand(leftRange..rightRange)
-     end
-   array
 end
+
+def is_int?(obj) 
+   obj = obj.to_i.to_s
+end
+ 
+def random_array(array)
+  p "Enter size: "
+  random=gets.chomp
+  is_int?(random)
+  while is_int?(random) != random.to_s do
+    p "Wrong input"
+    p "  U R IDIOT I CAN'T WORK WITH U !!!!!!"
+    exit
+  end
+  p "Enter random range from:"
+  leftRange=gets.chomp
+  p "to:"
+  rightRange=gets.chomp
+   
+  random=random.to_i
+  leftRange=leftRange.to_i
+  rightRange=rightRange.to_i
+   
+  random.times do
+  array<< rand(leftRange..rightRange)
+    end
+  array
+end
+
 p "RandomArray"
 array = []
-array = randomArray(array)
+array = random_array(array)
 buble=Sort.new(array)
 buble.bubleSort
 p buble.array
