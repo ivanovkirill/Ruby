5d08b9e2b24fde7f9819e98f240c158b02e06c6c
noneks <kirill_lp@list.ru>
2013-11-19 15:44:15 +0700

    Fix a bit again

--- app_pwgen.rb
+++ app_pwgen.rb
@@ -6,44 +6,45 @@ get '/' do
       <p><b>What passowrd do you want</b><Br>
     
       <input type="checkbox" name="@how_to_gen[]" value="1"> Choose this if you want to capitalize your passwrd<Br>
-      <input type="checkbox" name="@how_to_gen[]" value="2"> Choose this if you want to add some digits<Br>
-      <input type="checkbox" name="@how_to_gen[]" value="3"> Choose this if hate ambiguos characters<Br>
-      <input type="checkbox" name="@how_to_gen[]" value="4"> and the last one to generate random password<Br>
+      <input type="checkbox" name="@how_to_gen[]" value="1"> Choose this if you want to add some digits<Br>
+      <input type="checkbox" name="@how_to_gen[]" value="1"> Choose this if hate ambiguos characters<Br>
+      <input type="checkbox" name="@how_to_gen[]" value="1"> and the last one to generate random password<Br>
       
       <p><b>What is the length of the password do you want?(from 8 to 30)</p></b>
-      <p><input type="number" name="@password" size"3" min="8" max="30" required></p>
+      <p><input type="number" name="password" size"3" min="8" max="30" required></p>
       <p><input type="submit" value="Magic"></p>
      </form>
    HTML
 end
 
 post '/generate' do
-  @password_length = params['@password']
-  @how_to_gen = params[:@how_to_gen]
-  pwgen = 'pwgen '
-  @how_to_gen.to_s
-  #p @password_length
+  @password_length = params[:password]
+  @how_to_gen = params['@how_to_gen']
+  pwgen_string = 'pwgen '
+  p @password_length
   #p @how_to_gen
+  gen = ""
   if @password_length.to_i > 7 && @password_length.to_i < 31 
     @how_to_gen.each do |type|
       if type == "1"
-	gen_type = ' -n'
+	gen = ' -n'
       end
       if type == "2"
-	gen_type = ' -c'
+	gen = ' -c'
       end
       if type == "3"
-	gen_type = ' -B'
+	gen = ' -B'
       end
       if type == "4"
-	gen_type = ' -s'
+	gen = ' -s'
       end
     end
-    # i have no idea why i write with :
+    # i have no idea why i write this :
     # something wrong with gen_type
     # 
-    length_type = @password_length.to_s + " 1"
-    output=pwgen + gen_type
+    output = ""
+    length_type = @password_length + "1"
+    output=pwgen_string + gen
     output=output + length_type
     output = `#{output}`
     p "\nYour password is "+ output
